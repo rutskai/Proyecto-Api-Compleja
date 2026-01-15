1d81042122c7411717e9f0c7e044822e32553fac
Ruth <rcolladogarcia@iessonferrer.net>
2026-01-15 17:37:35 +0100

    Update loop in ShowQuiz.cs

--- Quiz-Movies/Functions/ShowQuiz.cs
+++ Quiz-Movies/Functions/ShowQuiz.cs
@@ -8,22 +8,33 @@ namespace Functions
     {
         public static async Task showQuiz()
         {
-            Movie? movie= await GetRandomMovieAsync();
-            if (movie is null || !IsValid(movie))
+            string option="";
+            while (option.ToLower()!="n")
             {
-                Console.WriteLine("No se ha podido acceder al contenido.");
-                return;
+
+
+                Movie? movie = await GetRandomMovieAsync();
+                if (movie is null || !IsValid(movie))
+                {
+                    Console.WriteLine("No se ha podido acceder al contenido.");
+                    return;
+                }
+
+                Console.WriteLine("==============================\nQUIZ DE PELÍCULAS DE LOS 90\n==============================");
+                Console.WriteLine("\nMini resumen:");
+                Console.WriteLine($"{movie.Summary}");
+                Console.WriteLine("\nCuál es el nombre de la película?");
+                string answer = Console.ReadLine() ?? "";
+
+                string result = await ValidateAnswer.Check(answer, movie);
+                Console.WriteLine(result);
+
+                Console.WriteLine("\nEscribe 'n' para salir, o cualquier otra letra para continuar:");
+                option=Console.ReadLine() ?? "";
+                
             }
 
-            Console.WriteLine("==============================\nQUIZ DE PELÍCULAS DE LOS 90\n==============================");
-            Console.WriteLine("\nMini resumen:");
-            Console.WriteLine($"{movie.Summary}");
-            Console.WriteLine("\nCuál es el nombre de la película?");
-            string answer= Console.ReadLine() ?? "";
-            
-            string result = await ValidateAnswer.Check(answer, movie);
-            Console.WriteLine(result);
-
+            Console.WriteLine("\nSaliendo del Quiz...\n");
 
         }
     }
